eaaae59ffccce5a5cf9211d1e95620e22d79f005
ZacH2C <zac.hellouin@gmail.com>
2020-05-13 16:08:34 +0200

    InitCommit

---
+++ mydevices.cpp
@@ -0,0 +1,62 @@
+#include "mydevices.h"
+
+#ifdef unix
+    #include <unistd.h>
+    #elif defined _WIN32
+    # include <windows.h>
+    #define sleep(x) Sleep(1000 * (x))
+#endif
+
+#include <pthread.h>
+
+using namespace std;
+
+//classe AnalogSensorTemperature
+AnalogSensorTemperature::AnalogSensorTemperature(int d,int  t):Device(),val(t),temps(d){
+  alea=1;
+}
+
+void AnalogSensorTemperature::run(){
+  while(1){
+    alea=1-alea;
+    if(ptrmem!=NULL)
+      *ptrmem=val+alea;
+    sleep(temps);
+  }
+}
+
+//classe DigitalActuatorLED
+DigitalActuatorLED::DigitalActuatorLED(int t):Device(),state(LOW),temps(t){
+}
+
+void DigitalActuatorLED::run(){
+  while(1){
+    if(ptrmem!=NULL)
+      state=*ptrmem;
+    if (state==LOW)
+      cout << "((((eteint))))\n";
+    else
+    cout << "((((allume))))\n";
+    sleep(temps);
+    }
+}
+
+// classe I2CActuatorScreen
+I2CActuatorScreen::I2CActuatorScreen ():Device(){
+  }
+
+void I2CActuatorScreen::run(){
+  while(1){
+    if ( (i2cbus!=NULL)&&!(i2cbus->isEmptyRegister(i2caddr))){
+      Device::i2cbus->requestFrom(i2caddr, buf, I2C_BUFFER_SIZE);
+      cout << "---screen :"<< buf << endl;
+    }
+    sleep(1);
+    }
+}
+
+
+
+
+
+
